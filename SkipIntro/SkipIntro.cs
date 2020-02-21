ad50257abed0dbb3103e611e131ffaa16f60ed94
Sheep-y <your.sheepy@gmail.com>
2020-02-21 13:47:33 +0800

    Revert to manual patch.

--- SkipIntro/SkipIntro.cs
+++ SkipIntro/SkipIntro.cs
@@ -12,31 +12,52 @@ using UnityEngine.Video;
 using System;
 using System.Reflection;
 using static System.Reflection.BindingFlags;
-using Base.Utils.GameConsole;
-using System.Globalization;
-
 
 namespace Sheepy.PhoenixPt_SkipIntro {
 
    public class Mod {
-      private static readonly Logger Log = new Logger( "Mods/SheepyMods.log" );
+      private static Logger Log = new Logger( "Mods/SheepyMods.log" );
 
       public static void Init () {
-         HarmonyInstance.Create( typeof( Mod ).Namespace ).PatchAll();
-         /*
+         HarmonyInstance harmony = HarmonyInstance.Create( typeof( Mod ).Namespace );
+         
          Patch( harmony, typeof( PhoenixGame ), "Initialize", "Prefix_Init" );
          Patch( harmony, typeof( PhoenixGame ), "BootCrt", "Prefix_Boot" );
          Patch( harmony, typeof( VideoPlaybackController ), "Play", null, "Postfix_Play" );
          //Patch( harmony, typeof( UIStateInitial ).GetMethod( "EnterState", NonPublic | Instance ), typeof( Mod ).GetMethod( "Prefix_Initial" ) );
-         */
+
+         Patch( harmony, typeof( UIStateHomeScreenCutscene ), "EnterState", null, "AfterHomeCutscene_Skip");
+         Patch( harmony, typeof( UIStateHomeScreenCutscene ), "PlayCutsceneOnFinishedLoad", "BeforeOnLoad_Skip" );
+
+         Patch( harmony, typeof( UIStateTacticalCutscene ), "EnterState", null, "AfterTacCutscene_Skip" );
+         Patch( harmony, typeof( UIStateTacticalCutscene ), "PlayCutsceneOnFinishedLoad", "BeforeOnLoad_Skip" );
+
+         Patch( harmony, typeof( LevelSwitchCurtainController ), "DropCurtainCrt", "BeforeDropCurtain_Skip" );
          // Disabled because it is a hassle to call OnCurtainLifted
          //Patch( harmony, typeof( LevelSwitchCurtainController ).GetMethod( "LiftCurtainCrt", Public | Instance ), typeof( Mod ).GetMethod( "Prefix_LiftCurtain" ) );
       }
 
-      public static bool LogError ( Exception ex ) {
+      #region Modding helpers
+      private static void Patch ( HarmonyInstance harmony, Type target, string toPatch, string prefix, string postfix = null ) {
+         Patch( harmony, target.GetMethod( toPatch, Public | NonPublic | Instance | Static ), prefix, postfix );
+      }
+
+      private static void Patch ( HarmonyInstance harmony, MethodInfo toPatch, string prefix, string postfix = null ) {
+         harmony.Patch( toPatch, ToHarmonyMethod( prefix ), ToHarmonyMethod( postfix ) );
+      }
+
+      private static HarmonyMethod ToHarmonyMethod ( string name ) {
+         if ( name == null ) return null;
+         MethodInfo func = typeof( Mod ).GetMethod( name );
+         if ( func == null ) throw new NullReferenceException( name + " is null" );
+         return new HarmonyMethod( func );
+      }
+
+      private static bool LogError ( Exception ex ) {
          //Log.Error( ex );
          return true;
       }
+      #endregion
 
       public static void Prefix_Init () { Log.Info( "Game Init" ); }
       public static void Prefix_Boot () { Log.Info( "Game Boot" ); }
@@ -51,6 +72,30 @@ namespace Sheepy.PhoenixPt_SkipIntro {
          return path.Contains( "Game_Intro_Cutscene" ) || path.Contains( "LandingSequences" );
       }
 
+      public static void AfterHomeCutscene_Skip ( UIStateHomeScreenCutscene __instance, VideoPlaybackSourceDef ____sourcePlaybackDef ) { try {
+         //UIModuleCutscenesPlayer player = (UIModuleCutscenesPlayer) typeof( UIStateHomeScreenCutscene ).GetProperty( "_cutscenePlayer", NonPublic | Instance ).GetValue( __instance );
+         //player?.VideoPlayer?.Stop();
+         if ( ShouldSkip( ____sourcePlaybackDef ) )
+            typeof( UIStateHomeScreenCutscene ).GetMethod( "OnCancel", NonPublic | Instance )?.Invoke( __instance, null );
+      } catch ( Exception ex ) { LogError( ex ); } }
+
+      public static void AfterTacCutscene_Skip ( UIStateTacticalCutscene __instance, VideoPlaybackSourceDef ____sourcePlaybackDef ) { try {
+         if ( ShouldSkip( ____sourcePlaybackDef ) )
+            typeof( UIStateTacticalCutscene ).GetMethod( "OnCancel", NonPublic | Instance )?.Invoke( __instance, null );
+      } catch ( Exception ex ) { LogError( ex ); } }
+
+      public static bool BeforeOnLoad_Skip ( VideoPlaybackSourceDef ____sourcePlaybackDef ) {
+         return ! ShouldSkip( ____sourcePlaybackDef );
+      }
+
+      public static bool BeforeDropCurtain_Skip ( ref IEnumerator<NextUpdate> __result, Action action, ref IUpdateable ____currentFadingRoutine ) { try {
+         ____currentFadingRoutine?.Stop();
+         action?.Invoke();
+         ____currentFadingRoutine = null;
+         __result = Enumerable.Empty<NextUpdate>().GetEnumerator();
+         return false;
+      } catch ( Exception ex ) { return LogError( ex ); } }
+
       /*
       public static bool Prefix_LiftCurtain ( ref IEnumerator<NextUpdate> __result, LevelSwitchCurtainController __instance ) { try {
          Action onCurtainLifted = __instance.OnCurtainLifted;
@@ -62,48 +107,5 @@ namespace Sheepy.PhoenixPt_SkipIntro {
          return true;
       } }
       */
-   }
-
-   [HarmonyPatch( typeof( UIStateHomeScreenCutscene ), "EnterState" )]
-   public static class UIStateHomeScreenCutscene_EnterState {
-      static void Postfix ( UIStateHomeScreenCutscene __instance, VideoPlaybackSourceDef ____sourcePlaybackDef ) { try {
-         //UIModuleCutscenesPlayer player = (UIModuleCutscenesPlayer) typeof( UIStateHomeScreenCutscene ).GetProperty( "_cutscenePlayer", NonPublic | Instance ).GetValue( __instance );
-         //player?.VideoPlayer?.Stop();
-         if ( Mod.ShouldSkip( ____sourcePlaybackDef ) )
-            typeof( UIStateHomeScreenCutscene ).GetMethod( "OnCancel", NonPublic | Instance )?.Invoke( __instance, null );
-      } catch ( Exception ex ) { Mod.LogError( ex ); } }
-   }
-
-   [HarmonyPatch( typeof( UIStateHomeScreenCutscene ), "PlayCutsceneOnFinishedLoad" )]
-   public static class UIStateHomeScreenCutscene_PlayCutsceneOnFinishedLoad {
-      static bool Prefix  ( VideoPlaybackSourceDef ____sourcePlaybackDef ) {
-         return ! Mod.ShouldSkip( ____sourcePlaybackDef );
-      }
-   }
-
-   [HarmonyPatch( typeof( UIStateTacticalCutscene ), "EnterState" )]
-   public static class UIStateTacticalCutscene_EnterState {
-      static void Postfix ( UIStateTacticalCutscene __instance, VideoPlaybackSourceDef ____sourcePlaybackDef ) { try {
-         if ( Mod.ShouldSkip( ____sourcePlaybackDef ) )
-            typeof( UIStateTacticalCutscene ).GetMethod( "OnCancel", NonPublic | Instance )?.Invoke( __instance, null );
-      } catch ( Exception ex ) { Mod.LogError( ex ); } }
-   }
-
-   [HarmonyPatch( typeof( UIStateTacticalCutscene ), "PlayCutsceneOnFinishedLoad" )]
-   public static class UIStateTacticalCutscene_PlayCutsceneOnFinishedLoad {
-      static bool Prefix ( VideoPlaybackSourceDef ____sourcePlaybackDef ) {
-         return ! Mod.ShouldSkip( ____sourcePlaybackDef );
-      }
-   }
-
-   [HarmonyPatch( typeof( LevelSwitchCurtainController ), "DropCurtainCrt" )]
-   public static class LevelSwitchCurtainController_DropCurtainCrt {
-      static bool Prefix ( ref IEnumerator<NextUpdate> __result, Action action, ref IUpdateable ____currentFadingRoutine ) { try {
-         ____currentFadingRoutine?.Stop();
-         action?.Invoke();
-         ____currentFadingRoutine = null;
-         __result = Enumerable.Empty<NextUpdate>().GetEnumerator();
-         return false;
-      } catch ( Exception ex ) { return Mod.LogError( ex ); } }
    }
 }
