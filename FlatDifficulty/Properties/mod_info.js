3e249b4481874c31024776417e1209c14c8f401d
Sheep-y <your.sheepy@gmail.com>
2020-02-27 08:34:14 +0800

    Add mod_info to Flat Difficulty.

---
+++ FlatDifficulty/Properties/mod_info.js
@@ -0,0 +1,12 @@
+({
+   Id: "Sheepy.FlatDifficulty",
+   Author: "Sheepy",
+   Name: {
+      en: "Flat Difficulty",
+      zh: "平坦難度" },
+   Description: {
+      en: "Disable Dynamic Difficulty System.\rPandorans strength only grow with time and not by player action.",
+      zh: "停用動態難度系統。\r潘朵拉民的強度只隨時間提升，不受玩家行動影響。" },
+   Url: {
+   },
+})
