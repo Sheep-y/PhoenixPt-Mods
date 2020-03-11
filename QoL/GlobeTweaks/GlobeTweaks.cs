10e0e9464109ea6f0090f87bcae733f59858d362
Sheep-y <your.sheepy@gmail.com>
2020-03-11 12:25:23 +0800

    Migrate NoAutoUnpause to new format.  Update assemble into and build scripts.

---
+++ QoL/GlobeTweaks/GlobeTweaks.cs
@@ -0,0 +1,46 @@
+﻿using Harmony;
+using PhoenixPoint.Geoscape.View;
+using PhoenixPoint.Geoscape.View.ViewStates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Reflection.BindingFlags;
+
+namespace Sheepy.PhoenixPt.GlobeTweaks {
+   class Settings {
+      public bool NoAutoUnpause = true;
+   }
+
+   class Mod : SheepyMod {
+      private static PropertyInfo ContextGetter;
+
+      public void Init () => new Mod().MainMod();
+
+      public void MainMod ( Settings settings = null ) {
+         if ( settings == null ) settings = new Settings();
+
+         if ( settings.NoAutoUnpause ) {
+            var ContextGetter = typeof( GeoscapeViewState ).GetProperty( "Context", NonPublic | Instance );
+            if ( ContextGetter != null )
+               Patch( typeof( UIStateVehicleSelected ), "AddTravelSite", nameof( Prefix_AddTravelSite ), nameof( Postfix_AddTravelSite ) );
+         }
+      }
+
+      private static GeoscapeViewContext getContext ( UIStateVehicleSelected __instance ) {
+         return (GeoscapeViewContext) ContextGetter.GetValue( __instance );
+      }
+
+      [ HarmonyPriority( Priority.VeryHigh ) ]
+      public static void Prefix_AddTravelSite ( UIStateVehicleSelected __instance, ref bool __state ) {
+         __state = getContext( __instance ).Level.Timing.Paused;
+      }
+
+      public static void Postfix_AddTravelSite ( UIStateVehicleSelected __instance, ref bool __state ) {
+         getContext( __instance ).Level.Timing.Paused = __state;
+      }
+   }
+}
--- QoL/GlobeTweaks/Properties/AssemblyInfo.cs
+++ QoL/GlobeTweaks/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GlobeTweaks")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle( "Globe Tweaks" )]
+[assembly: AssemblyDescription( "Phoenix Point Mod - Globe User Interface Tweaks" )]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GlobeTweaks")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCompany( "Sheepy" )]
+[assembly: AssemblyProduct( "" )]
+[assembly: AssemblyCopyright( "Public Domain" )]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion( "2.0.0.0" )]
+[assembly: AssemblyFileVersion( "2.0.0.0" )]
