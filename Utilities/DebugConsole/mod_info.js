e005b63e919ead83e65b998568485978a83159f8
Sheep-y <your.sheepy@gmail.com>
2020-03-18 16:59:14 +0800

    Add missing mod_info update.

--- Utilities/DebugConsole/mod_info.js
+++ Utilities/DebugConsole/mod_info.js
@@ -3,7 +3,9 @@
    Lang: "-",
    Taint: false,
    Name: "Debug Console",
-   Description: "Enable in-game console and log error to it.",
+   Description: "Enable in-game console and forward Unity log to it, errors included.",
+   Conflicts:
+      { Id: "ConsoleEnabler", Max: "1.0.0.0" },
    Url: {
       "GitHub" : "https://github.com/Sheep-y/PhoenixPt-Mods"
    },
