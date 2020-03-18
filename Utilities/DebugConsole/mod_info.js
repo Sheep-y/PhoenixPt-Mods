ee77695b19b51f2a556fb25176f7a6f0bc50702d
Sheep-y <your.sheepy@gmail.com>
2020-03-18 21:05:55 +0800

    Update descriptions and links.

--- Utilities/DebugConsole/mod_info.js
+++ Utilities/DebugConsole/mod_info.js
@@ -2,11 +2,15 @@
    Id: "Sheepy.DebugConsole",
    Lang: "-",
    Taint: false,
-   Name: "Debug Console",
-   Description: "Enable in-game console and forward Unity log to it, errors included.",
+   Name: {
+      en : "Debug Console",
+      zh : "除錯指令行" },
+   Description :
+      "Enable in-game console and route game log and unity log to it, showing game messages and errors, recorded in Console.log.",
    Conflicts:
       { Id: "ConsoleEnabler", Max: "1.0.0.0" },
    Url: {
+      "Nexus"  : "https://www.nexusmods.com/phoenixpoint/mods/44/",
       "GitHub" : "https://github.com/Sheep-y/PhoenixPt-Mods"
    },
 })
