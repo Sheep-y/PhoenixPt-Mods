7ab7da22c67399b5ad567477ae612990f85da7ab
Sheep-y <your.sheepy@gmail.com>
2020-03-23 00:46:24 +0800

    Ignore console write error. Example:
    18:4:0 | Error [ERROR] 14049 (283.024): Trying to add ConsoleLine(Clone) (UnityEngine.UI.Text) for graphic rebuild while we are already inside a graphic rebuild loop. This is not supported.

--- Utilities/DebugConsole/DebugConsole.cs
+++ Utilities/DebugConsole/DebugConsole.cs
@@ -10,12 +10,13 @@ namespace Sheepy.PhoenixPt.DebugConsole {
    public static class Mod {
       public static void Init () => SplashMod();
 
-      public static void SplashMod () {
+      public static void SplashMod ( Func< string, object, object > api = null ) {
          Application.logMessageReceived += UnityToConsole;
          GameConsoleWindow.DisableConsoleAccess = false;
       }
 
       private static void UnityToConsole ( string condition, string stackTrace, LogType type ) {
+         if ( type == LogType.Error && condition.Contains( "inside a graphic rebuild loop. This is not supported." ) ) return;
          GameConsoleWindow.Create().WriteLine( "{0} {1} {2}", type, condition, stackTrace );
       }
    }
