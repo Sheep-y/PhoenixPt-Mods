7c2662c12f874663eebde49db32fd6ea615f3c02
Sheep-y <your.sheepy@gmail.com>
2020-04-10 19:54:48 +0800

    Update to ConsoleCommand.

--- Unpublished/VersionText/Mod.cs
+++ Unpublished/VersionText/Mod.cs
@@ -1,4 +1,5 @@
 ﻿using Base.Build;
+using Base.Utils.GameConsole;
 using Harmony;
 using System;
 using System.Collections.Generic;
@@ -7,26 +8,46 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace VersionText {
-
    internal class ModConfig {
-      public string Text = "Hello World!";
-   }
+      public int ConfigVersion = 2;
+      public string Text { internal get; set; }
+      public string VersionText = "Hello";
+      public string BuildText = "World!";
 
+      internal ModConfig Update () {
+         if ( ConfigVersion < 2 ) {
+            VersionText = Text;
+            BuildText = "";
+            ConfigVersion = 2;
+            Mod.Api?.Invoke( "config save", this );
+         }
+         return this;
+      }
+   }
    public static class Mod {
+      internal static Func<string,object,object> Api;
       internal static ModConfig Config;
-      public static void MainMod ( Func<string,object,object> api ) {
-         Config = api?.Invoke( "config", typeof( ModConfig ) ) as ModConfig ?? new ModConfig();
+      public static void Init () => MainMod();
+      public static void MainMod ( Func<string,object,object> api = null ) {
+         Api = api;
+         Config = ( api?.Invoke( "config", typeof( ModConfig ) ) as ModConfig )?.Update() ?? new ModConfig();
          HarmonyInstance.Create( typeof( Mod ).Namespace ).PatchAll();
+      }
+
+      [ ConsoleCommand( Command = "VerText", Description = "Change version text" ) ]
+      static void ChangeText ( IConsole console, string ver, string build ) {
+         Mod.Config.VersionText = ver;
+         Mod.Config.BuildText = build;
       }
    }
 
-   [HarmonyPatch( typeof( RuntimeBuildInfo ), "Version", MethodType.Getter )]
+   [ HarmonyPatch( typeof( RuntimeBuildInfo ), "Version", MethodType.Getter ) ]
    static class RuntimeBuildInfoMod_GetVersion {
-      static void Postfix ( ref string __result ) => __result = Mod.Config.Text;
-   }
+      static void Postfix ( ref string __result ) => __result = Mod.Config.VersionText;
+   } 
 
-   [HarmonyPatch( typeof( RuntimeBuildInfo ), "Platform", MethodType.Getter )]
+   [ HarmonyPatch( typeof( RuntimeBuildInfo ), "Platform", MethodType.Getter ) ]
    static class RuntimeBuildInfoMod_GetPlatform {
-      static void Postfix ( ref string __result ) => __result = "";
+      static void Postfix ( ref string __result ) => __result = Mod.Config.BuildText;
    }
 }
--- Unpublished/VersionText/mod_info.js
+++ Unpublished/VersionText/mod_info.js
@@ -1,5 +1,11 @@
 ﻿{
     Id : "Sheepy.VersionText",
-    Description : "Replace in-game version text.",
+    Name : "Version Text",
+    Description : "A hello world mod.\r\t* Change home screen version text.\n\t* Configurable.",
+    Duration : "temp",
+    Url : { "GitHub" : "https://github.com/Sheep-y/PhoenixPt-Mods", "Nexus" : "https://nexusmods.com/phoenixpoint/mods/" },
+    LoadIndex : -10,
+    Requires : { Id: "Sheepy.DebugConsole", Min : "3.0" },
+    Disables : [ { Id : "DeafTheDead", Max : "0.9" }, {}, {} ],
     ConfigType : "VersionText.ModConfig",
 }
