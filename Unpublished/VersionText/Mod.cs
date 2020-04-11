c4350d4732888ac633db5961e7d8d6efc602903b
Sheep-y <your.sheepy@gmail.com>
2020-04-11 16:34:31 +0800

    Add lots of comments.

--- Unpublished/VersionText/Mod.cs
+++ Unpublished/VersionText/Mod.cs
@@ -6,39 +6,50 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine.UI;
 
 namespace VersionText {
+
+   /* Mod config class, saved as json through Newtonsoft Json.Net through Modnix api.
+    * The config class must be specfied as "ConfigType" in mod_info. 
+    * The initial values will be used as the default values. */
    internal class ModConfig {
-      public int ConfigVersion = 2;
-      public string Text { internal get; set; }
+      public int ConfigVersion = 2; // Having a "version" helps when you need to migrate from old config.
+      public string Text { internal get; set; } // This prop is legacy; it can be read (set) by Json.Net, but will not be saved (get).
       public string VersionText = "Hello";
       public string BuildText = "World!";
 
       internal ModConfig Update () {
-         if ( ConfigVersion < 2 ) {
-            VersionText = Text;
+         if ( ConfigVersion < 2 ) { // In this case we are moving Text to VersionText as a demo.
+            VersionText = Text;     // An alternative would be to split it between the two new fields.
             BuildText = "";
-            ConfigVersion = 2;
-            Mod.Api?.Invoke( "config save", this );
+            ConfigVersion = 2;      // Don't forget to update version!
+            Mod.Api?.Invoke( "config save", this ); // or to save the updated config!
          }
          return this;
       }
    }
 
+   /* Main mod class. The initialisers are here.
+    * Make sure it is public, non-abstract and non-nested. */
    public static class Mod {
-      internal static Func<string,object,object> Api;
-      internal static ModConfig Config;
-      public static void Init () => MainMod();
-      public static void MainMod ( Func<string,object,object> api = null ) {
-         Api = api;
+
+      internal static Func<string,object,object> Api; // Modnix api function, used by 3 out of 4 mod classes.
+      internal static ModConfig Config;               // Mod config, the whole mod need this to work.
+
+      public static void Init () => MainMod(); // PPML 0.1 and 0.3+ entry point. 0.2 not supported because of conflict.
+
+      /* Modnix entry point, Main Menu phase */
+      public static void MainMod ( Func<string, object, object> api = null ) {
+         // If Mod is made non-static, and created here, the fields could be made read-only, the first step to be thread-safe.
+         Api = api; // Below: read config from Modnix and update it, or use default config.
          Config = ( api?.Invoke( "config", typeof( ModConfig ) ) as ModConfig )?.Update() ?? new ModConfig();
          HarmonyInstance.Create( typeof( Mod ).Namespace ).PatchAll();
-         api?.Invoke( "api_add Sheepy.VerText", (Func<string,object,string>) ApiVerText );
+         api?.Invoke( "api_add Sheepy.VerText", (Func<string, object, string>) ApiVerText );
       }
 
+      /* Modnix API extension, registered in MainMod.
+       * Default: set version text.  With "build" specifier: set build text. */
       private static string ApiVerText ( string type, object arg ) {
          string old;
          if ( "build".Equals( type?.ToLowerInvariant() ) ) {
@@ -52,7 +63,8 @@ namespace VersionText {
          return old;
       }
 
-      [ ConsoleCommand( Command = "VerText", Description = "Set home screen version text." ) ]
+      /* Console command.  Depends on DebugConsole to load.  PPML 0.3+ may or may not work. */
+      [ConsoleCommand( Command = "VerText", Description = "Set home screen version text." )]
       public static void ConsoleVerText ( IConsole console, string ver, string build ) {
          console.WriteLine( Config.VersionText + " " + Config.BuildText );
          Config.VersionText = ver?.ToString();
@@ -61,37 +73,51 @@ namespace VersionText {
       }
    }
 
-   [ HarmonyPatch( typeof( UIModuleBuildRevision ), "SetRevisionNumber" ) ]
-   static class UIModuleBuildRevision_SetRevisionNumber {
+   /* Main patch.  For changing the version text. */
+   [HarmonyPatch( typeof( UIModuleBuildRevision ), "SetRevisionNumber" )]
+   internal static class UIModuleBuildRevision_SetRevisionNumber {
+
+      // This is the version text component, a Unity Text.
       private static Text VersionText;
-      static void Postfix ( UIModuleBuildRevision __instance ) {
+
+      // Save a copy of the text componet and change it.
+      private static void Postfix ( UIModuleBuildRevision __instance ) {
          VersionText = __instance.BuildRevisionNumber;
          RefreshText();
       }
 
+      // Actually change the text.  Also used by API extension and console command.
       internal static void RefreshText () {
+         // Abort if a change was requested before Postfix, e.g. use types a console command before main screen.
+         // Unity References are required to compare any unity component with null.
+         if ( VersionText == null ) return;
          VersionText.text = Mod.Config.VersionText + " " + Mod.Config.BuildText;
          UIStateMainMenu_EnterState.Refresh();
       }
    }
 
-   [ HarmonyPatch( typeof( UIStateMainMenu ), "EnterState" ) ]
+   /* Secondary patch.  For DebugConsole compatibility. */
+   [HarmonyPatch( typeof( UIStateMainMenu ), "EnterState" )]
    internal static class UIStateMainMenu_EnterState {
-      private static UIStateMainMenu MainMenu;
-      private static MethodBase DebugConsoleAdder;
 
+      private static UIStateMainMenu MainMenu; // Copy of home screen for DebugConsole call.
+      private static MethodBase DebugConsoleAdder; // DebugConsole method to call.
+
+      // Save a copy of the home screen and get mod method through reflection.
       private static void Postfix ( UIStateMainMenu __instance ) {
          try {
             var asm = Mod.Api?.Invoke( "assembly", "Sheepy.DebugConsole" ) as Assembly;
-            if ( asm == null ) return;
+            if ( asm == null ) return; // Abort if mod is not found.  e.g. When using PPML.
             MainMenu = __instance;
             DebugConsoleAdder = asm.GetType( "Sheepy.PhoenixPt.DebugConsole.Mod" )
                .GetMethod( "AfterMainMenu_AddModCount", BindingFlags.NonPublic | BindingFlags.Static );
          } catch ( Exception ex ) {
-            Mod.Api?.Invoke( "log error", ex );
+            // Duplicate exceptions will be ignored, when it is logged as the solo parameter.
+            Mod.Api?.Invoke( "log error", ex ); // "error" specifier is unnecessary, but makes our intention clear.
          }
       }
 
+      // Ask DebugConsole to do its job. Does not respect whether it is disabled, though.
       internal static void Refresh () => DebugConsoleAdder?.Invoke( null, new object[] { MainMenu } );
    }
 }
--- Unpublished/VersionText/mod_info.js
+++ Unpublished/VersionText/mod_info.js
@@ -1,11 +1,37 @@
-﻿{
-    Id : "Sheepy.VersionText",
-    Name : "Version Text",
-    Description : "A hello world mod.\r\t* Change home screen version text.\n\t* Configurable.",
-    Duration : "temp",
-    Url : { "GitHub" : "https://github.com/Sheep-y/PhoenixPt-Mods", "Nexus" : "https://nexusmods.com/phoenixpoint/mods/" },
-    LoadIndex : -10,
-    Requires : { Id: "Sheepy.DebugConsole", Min : "3.0" },
-    Disables : [ { Id : "DeafTheDead", Max : "0.9" }, {}, {} ],
-    ConfigType : "VersionText.ModConfig",
+﻿{  /* This file should be bundled with the dll, either zipped together or set to "Resource" or "Embedded Resource" in build action.
+      Not bundling this file means a number of features will fail to register with Modnix, such as config and dependencies. */
+
+   /* Mod Id.  Keep this unique and consistent. */
+   Id : "Sheepy.VersionText",
+
+   /* Yes comment is allowed.  This is not strict json. */
+   Name : "Version Text",
+
+   /* Plain text only please. */
+   Description : "A hello world mod.\r\t* Change home screen version text.\n\t* Configurable.",
+
+   /* temp = mod has no lasting impact on saves.  perm = saves may be severely impacted when mod is disabled. */
+   Duration : "temp",
+
+   /* Will be displayed in Modnix's info panel */
+   Url : {
+      "GitHub" : "https://github.com/Sheep-y/PhoenixPt-Mods",
+      "Nexus" : "https://nexusmods.com/phoenixpoint/mods/"
+   },
+
+   /* Default zero.  Lower index = loaded earlier.  Higher index = loaded later. */
+   LoadIndex : -10,
+
+   /* If any mod listed here is not found, this mod will be disabled. 
+      Depending on a mod does NOT mean your mod will be loaded after it.
+      Dependency does not affect load order (beyond disabling mods). */
+   Requires : { Id: "Sheepy.DebugConsole", Min : "3.0" },
+
+   /* Mods here will be disabled, if exists. */
+   Disables : [ { Id : "DeafTheDead", Max : "0.9" }, {}, {} ],
+
+   /* The dlls will be scanned for this type. If found, it will be used to manage config. */
+   ConfigType : "VersionText.ModConfig",
+
+   // Can also use single-line comment if you prefer
 }
