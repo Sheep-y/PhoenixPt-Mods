d0cca4cb80c6750abb415d1c7b9eee769346955b
Sheep-y <your.sheepy@gmail.com>
2020-04-14 01:15:38 +0800

    Add display and font size configs.

--- Geoscape/RecruitInfo/RecruitInfo.cs
+++ Geoscape/RecruitInfo/RecruitInfo.cs
@@ -10,11 +10,26 @@ using System.Linq;
 using UnityEngine;
 
 namespace Sheepy.PhoenixPt.RecruitInfo {
+
    internal class ModConfig {
-      public int Names_Font_Size = 42;
-      public int Names_Min_Font_Size = 10;
-      public int Hints_Font_Size = 42;
+      public DisplayConfig Display = new DisplayConfig();
+      public FontSizeConfig Font_Sizes = new FontSizeConfig();
       public int Config_Version = 20200413;
+   }
+
+   public class DisplayConfig {
+      public bool Main_Class_Desc = true;
+      public bool Personal_Skill_Names = true;
+      public bool Personal_Skill_Desc = true;
+
+      public bool Any_Desc => Main_Class_Desc || Personal_Skill_Desc;
+   }
+
+   public class FontSizeConfig {
+      public int Names = 42;
+      public int Names_Min = 10;
+      public int Desc_Title = 36;
+      public int Desc_Body = 30;
    }
 
    public class Mod : ZyMod {
@@ -33,6 +48,7 @@ namespace Sheepy.PhoenixPt.RecruitInfo {
          GameObject skillObj = owner.Find( "PersonalSkills" )?.gameObject;
 
          if ( skillObj != null ) return skillObj;
+         Info( "Creating Skill Text" );
 
          UnityEngine.UI.Text copyFrom = owner.Find( "RecruitCost" )?.Find( "Name" )?.GetComponent<UnityEngine.UI.Text>();
          if ( copyFrom == null ) copyFrom = ctl.RecruitName;
@@ -42,10 +58,9 @@ namespace Sheepy.PhoenixPt.RecruitInfo {
          UnityEngine.UI.Text text = skillObj.AddComponent<UnityEngine.UI.Text>();
          text.font = copyFrom.font;
          text.fontStyle = copyFrom.fontStyle; // RecruitName Bold, Cost normal
-         text.fontSize = Config.Names_Font_Size; // RecruitName 52, Cost 42
+         text.fontSize = Config.Font_Sizes.Names; // RecruitName 52, Cost 42
          text.resizeTextForBestFit = true;
-         text.resizeTextMinSize = Config.Names_Min_Font_Size;
-         //Log.Info( "{0} {1} {2}", copyFrom.font, copyFrom.fontSize, copyFrom.fontStyle );
+         text.resizeTextMinSize = Config.Font_Sizes.Names_Min;
 
          Transform rect = skillObj.GetComponent<Transform>();
          //rect.localPosition = new Vector3( 200, 0, 0 );
@@ -55,13 +70,15 @@ namespace Sheepy.PhoenixPt.RecruitInfo {
          outline.effectColor = Color.black;
          outline.effectDistance = new Vector2( 2, 2 );
 
-         skillObj.AddComponent<UITooltipText>();
+         if ( Config.Display.Any_Desc )
+            skillObj.AddComponent<UITooltipText>();
          skillObj.AddComponent<CanvasRenderer>();
          return skillObj;
       }
 
       private static string BuildHint ( LocalizedTextBind title, LocalizedTextBind desc ) {
-         return "<b>" + title.Localize() + "</b>\n" + desc.Localize();
+         return $"<size={Config.Font_Sizes.Desc_Title}><b>{title.Localize()}</b></size>\n" +
+                $"<size={Config.Font_Sizes.Desc_Body}>{desc.Localize()}</size>";
       }
 
       public static void AfterSetRecruitment_ListPerks ( HavenFacilityItemController __instance, GeoHaven ____haven ) { try {
@@ -77,15 +94,21 @@ namespace Sheepy.PhoenixPt.RecruitInfo {
             if ( track == null || track.Source != AbilityTrackSource.Personal ) continue;
             ViewElementDef mainClass = recruit.Progression.MainSpecDef.ViewElementDef;
             IEnumerable<ViewElementDef> abilities = track.AbilitiesByLevel.Where( e => e?.Ability != null ).Select( e => e.Ability.ViewElementDef );
-            string list = abilities.Select( e => TitleCase( e.DisplayName1.Localize() ) ).Join();
-            string hint = abilities.Select( e => BuildHint( e.DisplayName1, e.Description ) ).Join( null, "\n\n" );
-            skillText.GetComponent<UnityEngine.UI.Text>().text = "     " + list;
-            skillText.GetComponent<UITooltipText>().TipText = BuildHint( mainClass.DisplayName1, mainClass.DisplayName2 ) + "\n\n" + hint;
+            if ( Config.Display.Personal_Skill_Names ) {
+               string list = abilities.Select( e => TitleCase( e.DisplayName1.Localize() ) ).Join();
+               skillText.GetComponent<UnityEngine.UI.Text>().text = "     " + list;
+            }
+            if ( Config.Display.Any_Desc ) {
+               var tipText = "";
+               if ( Config.Display.Main_Class_Desc ) tipText += BuildHint( mainClass.DisplayName1, mainClass.DisplayName2 ) + "\n\n";
+               if ( Config.Display.Personal_Skill_Desc ) tipText += abilities.Select( e => BuildHint( e.DisplayName1, e.Description ) ).Join( null, "\n\n" );
+               skillText.GetComponent<UITooltipText>().TipText = tipText.Trim();
+            }
             skillText.SetActive( true );
             //skillText.GetComponent<UITooltipText>().UpdateText( hint );
             break;
          }
-         ModnixApi?.Invoke( "zy.gui.tree", __instance.gameObject );
+         //ModnixApi?.Invoke( "zy.ui.dump", __instance.gameObject );
       } catch ( Exception ex ) { Error( ex ); } }
    }
 }
