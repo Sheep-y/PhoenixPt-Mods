b49a80eba92cb89d57fb3a092dd1389886e6315c
Sheep-y <your.sheepy@gmail.com>
2020-04-13 12:46:48 +0800

    Disable old recruit info.

--- Geoscape/RecruitInfo/mod_info.js
+++ Geoscape/RecruitInfo/mod_info.js
@@ -1,19 +1,13 @@
 ({
    Id : "Sheepy.RecruitInfo",
-   Lang : "-",
+   Lang : "*",
    Duration : "temp",
    Name : "Recruit Info",
-   Description :
-      "Tune down the global faction war by reducing damage, number of attacks, and chance of successful attack.",
+   Description : "Show information on recruits such as personal skills and mutations.",
    Url : {
-      "Nexus" : "https://nexusmods.com/phoenixpoint/mods/24/",
+      "Nexus" : "https://nexusmods.com/phoenixpoint/mods/28/",
       "GitHub" : "https://github.com/Sheep-y/PhoenixPt-Mods/",
    },
-   Disables : [
-      { Id: "PhoenixPt_BetterDefence", Max: "1.0.0.0" },
-      { Id: "PhoenixPt_LessWar", Max: "1.0.0.0" },
-      { Id: "PhoenixPt_LimitWarToZone", Max: "1.0.0.0" },
-      { Id: "PhoenixPt_NoWar", Max: "1.0.0.0" },
-   ],
+   Disables : { Id: "PhoenixPt_RecruitInfo", Max: "1.0.0.0" },
    ConfigType : "Sheepy.PhoenixPt.RecruitInfo.ModConfig",
 })
