3bdf1074cec6c9ef58af95938f789294556bc7b8
Sheep-y <your.sheepy@gmail.com>
2020-04-14 01:54:01 +0800

    Refactor code and add display config validation.

--- Geoscape/RecruitInfo/RecruitInfo.cs
+++ Geoscape/RecruitInfo/RecruitInfo.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using static System.Diagnostics.TraceEventType;
 
 namespace Sheepy.PhoenixPt.RecruitInfo {
 
@@ -23,6 +24,12 @@ namespace Sheepy.PhoenixPt.RecruitInfo {
       public bool Personal_Skill_Desc = true;
 
       public bool Any_Desc => Main_Class_Desc || Personal_Skill_Desc;
+      internal void Validate () {
+         if ( ! Personal_Skill_Names && Main_Class_Desc )
+            Mod.Logger?.Invoke( Warning, "Main_Class_Desc will not work without Personal_Skill_Names", null );
+         if ( ! Personal_Skill_Names && Personal_Skill_Desc )
+            Mod.Logger?.Invoke( Warning, "Personal_Skill_Desc will not work without Personal_Skill_Names", null );
+      }
    }
 
    public class FontSizeConfig {
@@ -39,20 +46,21 @@ namespace Sheepy.PhoenixPt.RecruitInfo {
 
       public void MainMod ( Func< string, object, object > api = null ) {
          SetApi( api, out Config );
+         Config.Display.Validate();
          Patch( typeof( HavenFacilityItemController ), "SetRecruitmentGroup", null, "AfterSetRecruitment_ListPerks" );
       }
 
-      private static GameObject CreateSkillText ( HavenFacilityItemController ctl ) {
+      private static GameObject CreateNameText ( HavenFacilityItemController ctl, string name ) {
          Transform owner = ctl.RecruitName?.transform?.parent?.parent;
-         if ( owner == null ) throw new InvalidOperationException( "Recruit group not found, cannot add skill text" );
-         GameObject skillObj = owner.Find( "PersonalSkills" )?.gameObject;
+         if ( owner == null ) throw new InvalidOperationException( "Recruit group not found, cannot add recruit info text" );
+         GameObject skillObj = owner.Find( name )?.gameObject;
 
          if ( skillObj != null ) return skillObj;
-         Info( "Creating Skill Text" );
+         Info( "Create {0} Text", name );
 
          UnityEngine.UI.Text copyFrom = owner.Find( "RecruitCost" )?.Find( "Name" )?.GetComponent<UnityEngine.UI.Text>();
          if ( copyFrom == null ) copyFrom = ctl.RecruitName;
-         skillObj = new GameObject( "PersonalSkills" );
+         skillObj = new GameObject( name );
          skillObj.transform.SetParent( owner );
 
          UnityEngine.UI.Text text = skillObj.AddComponent<UnityEngine.UI.Text>();
@@ -82,22 +90,24 @@ namespace Sheepy.PhoenixPt.RecruitInfo {
       }
 
       public static void AfterSetRecruitment_ListPerks ( HavenFacilityItemController __instance, GeoHaven ____haven ) { try {
-         GameObject skillText = CreateSkillText( __instance );
          var recruit = ____haven?.AvailableRecruit;
+         if ( Config.Display.Personal_Skill_Names ) ShowPersonalSkills( __instance, recruit );
+         //ModnixApi?.Invoke( "zy.ui.dump", __instance.gameObject );
+      } catch ( Exception ex ) { Error( ex ); } }
+
+      public static void ShowPersonalSkills ( HavenFacilityItemController __instance, GeoCharacter recruit ) { try {
          var tracks = recruit?.Progression?.AbilityTracks;
+         GameObject skillText = CreateNameText( __instance, "PersonalSkills" );
          if ( tracks == null ) {
             skillText.SetActive( false );
             return;
          }
-
          foreach ( var track in tracks ) {
             if ( track == null || track.Source != AbilityTrackSource.Personal ) continue;
             ViewElementDef mainClass = recruit.Progression.MainSpecDef.ViewElementDef;
             IEnumerable<ViewElementDef> abilities = track.AbilitiesByLevel.Where( e => e?.Ability != null ).Select( e => e.Ability.ViewElementDef );
-            if ( Config.Display.Personal_Skill_Names ) {
-               string list = abilities.Select( e => TitleCase( e.DisplayName1.Localize() ) ).Join();
-               skillText.GetComponent<UnityEngine.UI.Text>().text = "     " + list;
-            }
+            string list = abilities.Select( e => TitleCase( e.DisplayName1.Localize() ) ).Join();
+            skillText.GetComponent<UnityEngine.UI.Text>().text = "     " + list;
             if ( Config.Display.Any_Desc ) {
                var tipText = "";
                if ( Config.Display.Main_Class_Desc ) tipText += BuildHint( mainClass.DisplayName1, mainClass.DisplayName2 ) + "\n\n";
@@ -108,7 +118,6 @@ namespace Sheepy.PhoenixPt.RecruitInfo {
             //skillText.GetComponent<UITooltipText>().UpdateText( hint );
             break;
          }
-         //ModnixApi?.Invoke( "zy.ui.dump", __instance.gameObject );
       } catch ( Exception ex ) { Error( ex ); } }
    }
 }
