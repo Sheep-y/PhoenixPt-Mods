e698392cc783ecf276f62fae4ad18170038e2c1d
Sheep-y <your.sheepy@gmail.com>
2020-04-22 13:04:24 +0800

    New mod "Traditional Chinese".

---
+++ Utilities/TraditionalChinese/Zht.cs
@@ -0,0 +1,18 @@
+﻿using Harmony;
+using I2.Loc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sheepy.PhoenixPt.Zht {
+    public class Mod {
+      public static void Init () => new Mod().SplashMod();
+
+      public void SplashMod ( Func< string, object, object > api = null ) {
+         api( "log", LocalizationManager.CurrentLanguageCode );
+         //HarmonyInstance.Create( typeof( Mod ).Namespace ).Patch( );
+      }
+    }
+}
---
+++ Utilities/TraditionalChinese/mod_info.js
@@ -0,0 +1,16 @@
+({
+   Id : "Sheepy.Zht",
+   LoadIndex : -100,
+   Lang : "zh",
+   Duration : "temp",
+   Name : {
+      en : "Traditional Chinese",
+      zh : "正體中文" },
+   Description : {
+      en : "Convert the game's Simplified Chinese to Traditional Chinese.",
+      zh : "將簡體中文轉為正體。" },
+   Url : {
+      "Nexus" : "https://nexusmods.com/phoenixpoint/mods/17/",
+      "GitHub" : "https://github.com/Sheep-y/PhoenixPt-Mods/",
+   },
+})
