ec29c0412399143cf496d8df79e42fc32814d65f
Sheep-y <your.sheepy@gmail.com>
2020-05-01 16:15:12 +0800

    Add Modnix version requirement because of api return type.

--- Utilities/DebugConsole/mod_info.js
+++ Utilities/DebugConsole/mod_info.js
@@ -1,26 +1,28 @@
 ({
-   Id: "Sheepy.DebugConsole",
+   Id : "Sheepy.DebugConsole",
    LoadIndex : -500,
 
-   Lang: "en",
+   Lang : "en",
    Duration : "temp",
-   Name: {
+   Name : {
       en : "Debug Console",
       zh : "除錯指令行" },
    Description :
 "Enable in-game console and route game log, unity log, and modnix log to it, showing game messages and errors, recorded in Console.log.  Provide console and api extension that aids in mod development.
 
 Errors and warnings caused from using Modinx API through console will be picked up by Modnix and reported. Modnix does not make an exception for this mod and the mod itself should not cause errors or warnings.",
-   Disables:
+   Requires :
+      { Id: "Modnix", Max: "2.3" },
+   Disables :
       { Id: "ConsoleEnabler", Max: "1.0.0.0" },
-   Url: {
+   Url : {
       "Nexus"  : "https://nexusmods.com/phoenixpoint/mods/44/",
       "GitHub" : "https://github.com/Sheep-y/PhoenixPt-Mods/"
    },
-   ConfigType: "Sheepy.PhoenixPt.DebugConsole.ModConfig",
+   ConfigType : "Sheepy.PhoenixPt.DebugConsole.ModConfig",
 
    /* Legacy config for Modnix 1.0 */
-   DefaultConfig: {
+   DefaultConfig : {
       "Mod_Count_In_Version" : true,
       "Log_Game_Error" : true,
       "Log_Game_Info" : true,
