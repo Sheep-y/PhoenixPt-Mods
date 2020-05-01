1e74f7a2efef5d679879ac9e49e277b18178b250
Sheep-y <your.sheepy@gmail.com>
2020-05-01 16:23:32 +0800

    Cancelling requires because most of the mod still work. Also remove config compat with Modnix 1.

--- Utilities/DebugConsole/mod_info.js
+++ Utilities/DebugConsole/mod_info.js
@@ -11,8 +11,6 @@
 "Enable in-game console and route game log, unity log, and modnix log to it, showing game messages and errors, recorded in Console.log.  Provide console and api extension that aids in mod development.
 
 Errors and warnings caused from using Modinx API through console will be picked up by Modnix and reported. Modnix does not make an exception for this mod and the mod itself should not cause errors or warnings.",
-   Requires :
-      { Id: "Modnix", Max: "2.3" },
    Disables :
       { Id: "ConsoleEnabler", Max: "1.0.0.0" },
    Url : {
@@ -20,17 +18,4 @@ Errors and warnings caused from using Modinx API through console will be picked 
       "GitHub" : "https://github.com/Sheep-y/PhoenixPt-Mods/"
    },
    ConfigType : "Sheepy.PhoenixPt.DebugConsole.ModConfig",
-
-   /* Legacy config for Modnix 1.0 */
-   DefaultConfig : {
-      "Mod_Count_In_Version" : true,
-      "Log_Game_Error" : true,
-      "Log_Game_Info" : true,
-      "Log_Modnix_Error" : true,
-      "Log_Modnix_Info" : true,
-      "Log_Modnix_Verbose" : false,
-      "Scan_Mods_For_Command" : true,
-      "Write_Modnix_To_Console_Logfile" : false,
-      "Config_Version" : 20200405,
-   },
 })
