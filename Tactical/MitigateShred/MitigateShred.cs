a69d8d030f151fc1cef42a71594f83e98860e20d
Sheep-y <your.sheepy@gmail.com>
2020-05-01 16:14:49 +0800

    Migrate to ZyMod.

--- Tactical/MitigateShred/MitigateShred.cs
+++ Tactical/MitigateShred/MitigateShred.cs
@@ -1,46 +1,26 @@
 ﻿using System;
-using System.Reflection;
+using PhoenixPoint.Tactical.Entities;
 
-namespace Sheepy.PhoenixPt_MitigateShred {
-   using Harmony;
-   using PhoenixPoint.Tactical.Entities;
-   using static System.Reflection.BindingFlags;
+namespace Sheepy.PhoenixPt.MitigateShred {
 
-   public class Mod {
-      //private static Logging.Logger Log = new Logging.Logger( "Mods/SheepyMods.log" );
+   internal class ModConfig {
+      public float Convert_Ratio = 0.1f;
+   }
 
-      public static void Init () {
-         HarmonyInstance harmony = HarmonyInstance.Create( typeof( Mod ).Namespace );
+   public class Mod : ZyMod {
+      internal static ModConfig Config;
 
-         Patch( harmony, typeof( DamageAccumulation ), "GenerateStandardDamageTargetData", null, "AfterStandardDamage_Shred" );
+      public static void Init () => new Mod().MainMod();
+
+      public void MainMod ( Func< string, object, object > api = null ) {
+         SetApi( api, out Config );
+         Patch( typeof( DamageAccumulation ), "GenerateStandardDamageTargetData", null, "AfterStandardDamage_Shred" );
       }
-
-      #region Modding helpers
-      private static void Patch ( HarmonyInstance harmony, Type target, string toPatch, string prefix, string postfix = null ) {
-         Patch( harmony, target.GetMethod( toPatch, Public | NonPublic | Instance | Static ), prefix, postfix );
-      }
-
-      private static void Patch ( HarmonyInstance harmony, MethodInfo toPatch, string prefix, string postfix = null ) {
-         harmony.Patch( toPatch, ToHarmonyMethod( prefix ), ToHarmonyMethod( postfix ) );
-      }
-
-      private static HarmonyMethod ToHarmonyMethod ( string name ) {
-         if ( name == null ) return null;
-         MethodInfo func = typeof( Mod ).GetMethod( name );
-         if ( func == null ) throw new NullReferenceException( name + " is null" );
-         return new HarmonyMethod( func );
-      }
-
-      private static bool LogError ( Exception ex ) {
-         //Log.Error( ex );
-         return true;
-      }
-      #endregion
 
       public static void AfterStandardDamage_Shred ( DamageAccumulation.TargetData __result ) { try {
          float mitigated = __result.DamageResult.ArmorMitigatedDamage;
          if ( mitigated > 0 )
-            __result.DamageResult.ArmorDamage += (float) Math.Max( 1, Math.Round( mitigated / 10 ) );
-      } catch ( Exception ex ) { LogError( ex ); } }
+            __result.DamageResult.ArmorDamage += (float) Math.Max( 1, Math.Round( mitigated * Config.Convert_Ratio ) );
+      } catch ( Exception ex ) { Error( ex ); } }
    }
 }
