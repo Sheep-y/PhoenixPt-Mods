7cb692d5e02c60ba8d85568f71158d501638fa33
Sheep-y <your.sheepy@gmail.com>
2020-05-03 00:13:40 +0800

    Switch to the new ZyMod.

--- BlockTelemetry/BlockTelemetry.cs
+++ BlockTelemetry/BlockTelemetry.cs
@@ -1,41 +1,24 @@
-﻿using Harmony;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System;
 using System.Linq;
 using System.Reflection;
 
 namespace Sheepy.PhoenixPt.BlockTelemetry {
 
-   public static class Mod {
+   public class Mod : ZyMod {
 
-      internal static Func<string,object,object> Api;
+      public static void Init () => new Mod().SplashMod();
 
-      public static void Init () => SplashMod();
-
-      public static void SplashMod ( Func<string, object, object> api = null ) {
-         var type = ( api( "assembly", "game" ) as Assembly ??
-            AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault( e => e.FullName.StartsWith( "Assembly-CSharp," ) ) )
-            ?.GetType( "AmplitudeWebClient" );
+      public void SplashMod ( Func<string, object, object> api = null ) {
+         SetApi( api );
+         var type = GameAssembly?.GetType( "AmplitudeWebClient" );
          if ( type == null ) {
-            api?.Invoke( "log error", "AmplitudeWebClient not found." );
+            Api( "log error", "AmplitudeWebClient not found." );
             return;
          }
 
-         Api = api;
-         var harmony = HarmonyInstance.Create( typeof( Mod ).Namespace );
-
-         api?.Invoke( "log", "Force disabling WebClient." );
-         harmony.Patch( type.GetMethod( "get_Enabled" ),
-            new HarmonyMethod( typeof( Mod ).GetMethod( nameof( DisableAll ), BindingFlags.NonPublic | BindingFlags.Static ) ) );
-
-         api?.Invoke( "log", "Force disabling WebClient.UserHasAllowed." );
-         harmony.Patch( type.GetMethod( "get_UserHasAllowed" ),
-            new HarmonyMethod( typeof( Mod ).GetMethod( nameof( DisableAll ), BindingFlags.NonPublic | BindingFlags.Static ) ) );
-
-         api?.Invoke( "log", "Blocking telemetry events." );
-         harmony.Patch( type.GetMethod( "SendEventTask", BindingFlags.NonPublic | BindingFlags.Instance ),
-            new HarmonyMethod( typeof( Mod ).GetMethod( nameof( BlockSendEvent ), BindingFlags.NonPublic | BindingFlags.Static ) ) );
+         TryPatch( type, "get_Enabled", nameof( DisableAll ) );
+         TryPatch( type, "get_UserHasAllowed", nameof( DisableAll ) );
+         TryPatch( type, "SendEventTask", nameof( BlockSendEvent ) );
       }
 
       private static bool DisableAll ( ref bool __result ) {
@@ -44,7 +27,7 @@ namespace Sheepy.PhoenixPt.BlockTelemetry {
       }
 
       private static bool BlockSendEvent () {
-         Api?.Invoke( "log info", new ApplicationException( "Blocking unexpected telemetry" ) );
+         Api( "log info", new ApplicationException( "Blocking unexpected telemetry." ) );
          return false;
       }
    }
