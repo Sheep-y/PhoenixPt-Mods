1e0d5e78a0e3400f020c451d187a7321f789715d
Sheep-y <your.sheepy@gmail.com>
2020-05-02 21:50:54 +0800

    New mod "Block Telemetry".

---
+++ BlockTelemetry/changelog.md
@@ -0,0 +1,5 @@
+Changelog of Block Telemetry, a Phoenix Point Mod by Sheepy
+
+# Version 1, 2020-05-02
+
+* Force disable telemetry and block telemetry events.
---
+++ BlockTelemetry/mod_info.js
@@ -0,0 +1,11 @@
+({
+   Id : "Sheepy.BlockTelemetry",
+   Lang : "-",
+   Duration : "temp",
+   Name : "Block Telemetry",
+   Description : "Force disable telemetry and block any telemetry event that ignore the flags",
+   Url: {
+      "Nexus"  : "https://nexusmods.com/phoenixpoint/mods/45/",
+      "GitHub" : "https://github.com/Sheep-y/PhoenixPt-Mods/"
+   },
+})
