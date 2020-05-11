206cbd3bd51737db095d2593564b433e5dace427
Sheep-y <your.sheepy@gmail.com>
2020-05-11 15:43:11 +0800

    Add mod_info.js

---
+++ Utilities/ScriptingLibrary/mod_info.js
@@ -0,0 +1,11 @@
+({
+   Id : "Sheepy.Scripting",
+   Flags : "Library",
+   Name : "Scripting Library",
+   Description : 'Support "Eval" mod actions.',
+   Url : {
+      "Nexus" : "https://nexusmods.com/phoenixpoint/mods/26/",
+      "GitHub" : "https://github.com/Sheep-y/PhoenixPt-Mods/",
+   },
+   Requires : { Id: "Modnix", Min: 3 },
+})
