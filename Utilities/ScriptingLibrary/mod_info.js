5416c8e78a6cc5e81aee8fabab0d36414b6058b4
Sheep-y <your.sheepy@gmail.com>
2020-05-20 21:23:08 +0800

    Remove requires and flags, since it can still provide console Eval.

--- Utilities/ScriptingLibrary/mod_info.js
+++ Utilities/ScriptingLibrary/mod_info.js
@@ -1,12 +1,10 @@
 ({
    Id : "Zy.Scripting",
-   Flags : "Library",
    Name : "Scripting Library",
-   Description : 'Support "Eval" mod actions, "Eval" console command, "eval.cs" api.',
+   Description : 'Support "Eval" mod actions, "Eval" console command, "eval.cs" api, "pp.def" and "pp.defs" api.',
    LoadIndex : -200, // Load early to pre-load scripting engine
    Url : {
       "Nexus" : "https://nexusmods.com/phoenixpoint/mods/",
       "GitHub" : "https://github.com/Sheep-y/PhoenixPt-Mods/",
    },
-   Requires : { Id: "Modnix", Min: 3 },
 })
