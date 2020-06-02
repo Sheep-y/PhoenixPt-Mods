42d376615f17a738da2ce4d05de4962612be208d
Sheep-y <your.sheepy@gmail.com>
2020-06-02 22:54:07 +0800

    Drop version to reflect its dev status.

--- Utilities/ScriptingLibrary/mod_info.js
+++ Utilities/ScriptingLibrary/mod_info.js
@@ -1,7 +1,7 @@
 ({
    Id : "Zy.Scripting",
-   Name : "Scripting Library",
-   Description : 'Supports runtime C# evaluation, such as "Eval" mod actions, "Eval" console command, and "eval.cs" api.\r\rProvides data lookup by name and path through "pp.def" and "pp.defs" api.\r\rMods may also directly import helpers from ScriptHelpers.',
+   Name : "Scripting Library (Beta)",
+   Description : 'Supports runtime C# evaluation, such as "Eval" mod actions (Modnix 3), "Eval" console command, and "eval.cs" api.\r\rProvides data lookup by name and path through "pp.def" and "pp.defs" api.\r\rMods may also directly import helpers from ScriptHelpers.',
    LoadIndex : -200, // Load early to pre-load scripting engine
    Url : {
       "Nexus" : "https://nexusmods.com/phoenixpoint/mods/",
