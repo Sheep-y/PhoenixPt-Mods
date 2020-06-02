90470cfbd250798ed41a52b62359dfd6efbbcfe8
Sheep-y <your.sheepy@gmail.com>
2020-06-02 22:51:51 +0800

    Add mod comments.

--- Tactical/LaserOnFire/LaserOnFire.js
+++ Tactical/LaserOnFire/LaserOnFire.js
@@ -3,15 +3,19 @@
    Name : "Laser on Fire",
    Author : "Sheepy",
    Version : "1.0",
-   Description : "Add a small fire damage to all laser weapons, plus pierce or shred on a few lasers.\nAlso modify Destiny III to fire in first person.\n\nThis mod requires Modnix 3+ and the Scripting Library mod. Tested on Phoenix Point Ver 1.0.57630.",
+   Description : "Add a small fire damage to laser weapons, plus pierce or shred on a few lasers.\nAlso modify Destiny III to fire in first person.\n\nThis mod requires Modnix 3+ and the Scripting Library mod. Tested on Phoenix Point Ver 1.0.57630.",
    Url : {
       "Nexus" : "https://nexusmods.com/phoenixpoint/mods/33/",
       "GitHub" : "https://github.com/Sheep-y/PhoenixPt-Mods/",
    },
    Requires: "Zy.Scripting",
    Actions : [{
-      "Action"  : "Default",
-      "OnError" : "log, stop"
+      // Set the defaults for subsequence properties
+      "Action" : "Default",
+      // By default, action triggers when the game switches to geoscape or tactical for the first time.
+      //"Phase" : "GameMod",
+      // Default error handling is "log, continue", which log at error level.  Change "continue" to "stop" to stop on error.
+      "OnError" : "warn, continue"
    },{ // Add fire to Hephaestus II (Synedrion Laser Pistol)
       "Eval" : 'GetDef<WeaponDef>( "a30138e8-a29b-1f24-8b80-7dafe1b9aada" ).Fire( 1 )',
    },{ // Add fire to Deimos AR-L (Synedrion Laser Assault Rifle)
@@ -25,7 +29,7 @@
    },{ // Add fire to Destiny III (Phoenix Laser Array), and assign to pla.  ';' is required for assignment.
       "Eval" : 'var pla = GetDef<WeaponDef>( "fd84633b-79a3-82e4-fb53-07ab8c778545" ).Fire( 2 );',
    },{ // Enable Destiny III first-person aim by replacing launch rocket ability with fire weapon ability,
-      "Eval" : 'pla.Abilities = pla.Abilities.Where( e => e.Guid != "0ea4735b-dde8-e904-180e-3e1faef16a56" )
+      "Eval" : 'pla.Abilities = pla.Abilities.Where( e => e.Guid != "0ea4735b-dde8-e904-180e-3e1faef16a56" ) // (Multi-line is ok)
            .Concat( new AbilityDef[] { Repo.GetDef( "d3e8b389-069f-04c4-8aca-fb204c74fd37" ) as AbilityDef } ).ToArray()',
    },{ // and disable first person camera shake
       "Eval" : 'pla.DamagePayload.CameraShake = null',
