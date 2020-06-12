779fe8a63b3264f185c718dd90d9f3a694595291
Sheep-y <your.sheepy@gmail.com>
2020-06-13 02:08:13 +0800

    Defer release by a day.

--- Utilities/DumpInfo/changelog.md
+++ Utilities/DumpInfo/changelog.md
@@ -1,6 +1,6 @@
 Changelog of Dump Info, a Phoenix Point Mod by Sheepy
 
-# Version 1, 2020-06-12
+# Version 1, 2020-06-13
 
 * Mod is designed for Modnix 3; other loaders may yield incomplete data.
 * Tested on Phoenix Point 1.0.57630 with Modnix 3.
--- Utilities/DumpInfo/mod_info.js
+++ Utilities/DumpInfo/mod_info.js
@@ -6,6 +6,7 @@
    Description : "Export various data to xml/csv when you start or load a geoscape game.  Note that Dump_Others requires Scripting Library.\n\nData are saved in this mod's folder by default - click the path below to open the folder.",
    Url : {
       "GitHub" : "https://github.com/Sheep-y/PhoenixPt-Mods/"
+      "Nexus"  : "https://nexusmods.com/phoenixpoint/mods/50/",
    },
    ConfigType : "Sheepy.PhoenixPt.DumpInfo.ModConfig",
 })
