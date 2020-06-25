59499a3c2cd3dd467aed2d436f180d3d84c759d5
Sheep-y <your.sheepy@gmail.com>
2020-06-25 10:11:21 +0800

    Move to GeoscapeMod, fix typo, better description.

--- Unpublished/LessRecruit/LessRecruit.js
+++ Unpublished/LessRecruit/LessRecruit.js
@@ -1,14 +1,17 @@
 ({
-   Id : "Sheepy.LessRecriot",
+   Id : "Sheepy.LessRecruit",
    Name : "Less Recruit",
    Duration: "newgame",
    Lang : "-",
    Author : "Sheepy",
    Version : "0.0.2020.0625",
-   Description : "Remove 1 Assault from the starting squad of each difficulty, leaving 5 people for Rookie, 4 for Veteran, and 3 for Hero & Legend.",
+   Description : "Remove 1 Assault from the starting squad of each difficulty, leaving 5 people for Rookie, 4 for Veteran, and 3 for Hero & Legend.\n\nRequires Scripting Library (C#).",
    Url : "https://github.com/Sheep-y/PhoenixPt-Mods/",
    Requires: "Zy.cSharp",
    Actions : [{
+      "Action" : "Default",
+      "Phase" : "GeoscapeMod",
+   },{
       "Eval" : 'void LessRecruit ( string guid ) { ref var ary = ref GetDef<GameDifficultyLevelDef>( guid ).StartingSquadTemplate; ary = ary.Take( ary.Length - 1 ).ToArray(); }',
    },{
       "Eval" : 'LessRecruit( "Easy_GameDifficultyLevelDef" )', // Rookie
