32ec7e797cb1281b91698ba29aa9a9f608dd7a6c
Sheep-y <your.sheepy@gmail.com>
2020-07-01 06:09:08 +0800

    Try to force drop armour, also failed. Error: "Trying to get AimPoint while {0} is not active/visible."

--- Tactical/LootEverything/LootEverything.cs
+++ Tactical/LootEverything/LootEverything.cs
@@ -1,16 +1,18 @@
 ﻿using System;
-using Base.Core;
-using Base.Defs;
+using System.Collections.Generic;
+using System.Linq;
 using PhoenixPoint.Common.Core;
 using PhoenixPoint.Common.Entities.GameTags;
+using PhoenixPoint.Common.Entities.Items;
+using PhoenixPoint.Geoscape.Entities;
+using PhoenixPoint.Tactical.Entities.Abilities;
 using PhoenixPoint.Tactical.Entities.Equipments;
 
 namespace Sheepy.PhoenixPt.LootEverything {
 
    internal class ModConfig {
-      public ushort Min_Item_Drop_Percentage = 100;
-      public ushort Armour_Drop_Percentage = 100;
-      public ushort Mount_Drop_Percentage = 100;
+      public bool Drop_All_Item = true;
+      public bool Drop_All_Armour = true;
       //public bool Stript_Survivors = true;
       public int Config_Version = 20200630;
    }
@@ -24,45 +26,44 @@ namespace Sheepy.PhoenixPt.LootEverything {
 
       public void TacticalMod ( Func< string, object, object > api = null ) {
          SetApi( api, out Config );
-         MakeItemDroppable();
-      }
-
-      private static void MakeItemDroppable () {
-         var gameTags = SharedData.GetSharedDataFromGame().SharedGameTags;
-         GameTagDef armour = gameTags.ArmorTag, manu = gameTags.ManufacturableTag, mount = gameTags.MountedTag;
-         int maxItemDestroyChance = Math.Min( Math.Max( 0, 100 - Config.Min_Item_Drop_Percentage ), 100 );
-
-         foreach ( var item in GameUtl.GameComponent< DefRepository >().GetAllDefs<TacticalItemDef>() ) {
-            var tags = item.Tags;
-            if ( tags == null || tags.Count == 0 || ! tags.Contains( manu ) || item.IsPermanentAugment ) continue;
-            if ( tags.Contains( armour ) )
-               MakeItemDroppable( item, item.IsMountedToBody ? Config.Mount_Drop_Percentage : Config.Armour_Drop_Percentage );
-            else if ( tags.Contains( mount ) )
-               MakeItemDroppable( item, Config.Mount_Drop_Percentage );
-            else if ( item.DropOnActorDeath && item.DestroyOnActorDeathPerc > maxItemDestroyChance )
-               MakeItemDroppable( item, Config.Min_Item_Drop_Percentage );
+         TryPatch( typeof( DieAbility ), "ShouldDestroyItem", postfix: nameof( AfterShouldDestroyItem_KeepItem ) );
+         if ( Config.Drop_All_Armour ) {
+            TryPatch( typeof( DieAbility ), "DropItems", postfix: nameof( AfterDropItems_DropArmour ) );
+            TryPatch( typeof( GeoMission ), "GetDeadSquadMembersArmour", nameof( OverrideGetDeadSquadMembersArmour_Disable ) );
          }
       }
 
-      private static void MakeItemDroppable ( TacticalItemDef item, int perc ) {
-         int killPerc = Math.Max( 0, 100 - perc );
-         if ( killPerc >= 100 ) return;
-         item.DropOnActorDeath = true;
-         if ( item.DestroyOnActorDeathPerc > killPerc )
-            item.DestroyOnActorDeathPerc = killPerc;
-         Api( "log v", $"Set {item.name} drop% = {item.DestroyOnActorDeathPerc}" );
-      }
-
-      /*
-      TryPatch( typeof( DieAbility ), "ShouldDestroyItem", postfix: nameof( AfterShouldDestroyItem_KeepItem ) );
       public static void AfterShouldDestroyItem_KeepItem ( ref bool __result, TacticalItem item ) {
          if ( ! __result ) return;
-         __result = false;
-         try {
-            Api( "log v", $"Force dropping {item.TacticalItemDef.ViewElementDef.Name}" );
+         if ( Config.Drop_All_Item ) try {
+            __result = false;
+            Api( "log", $"Force dropping {item.TacticalItemDef.ViewElementDef.Name}" );
          } catch ( Exception ) { }
       }
 
+      public static bool OverrideGetDeadSquadMembersArmour_Disable ( ref IEnumerable<GeoItem> __result ) {
+         __result = Enumerable.Empty<GeoItem>();
+         return false;
+      }
+
+      public static void AfterDropItems_DropArmour ( DieAbility __instance ) {
+         var shared = SharedData.GetSharedDataFromGame();
+         var gameTags = shared.SharedGameTags;
+         var actor = __instance.TacticalActor;
+         GameTagDef armour = gameTags.ArmorTag, manu = gameTags.ManufacturableTag, mount = gameTags.MountedTag;
+
+         foreach ( var item in actor.BodyState.GetArmourItems() ) {
+            var def = item.ItemDef as TacticalItemDef;
+            var tags = def?.Tags;
+            if ( tags == null || tags.Count == 0 || ! tags.Contains( manu ) || def.IsPermanentAugment ) continue;
+            if ( tags.Contains( armour ) || tags.Contains( mount ) ) {
+               Api( "log", $"Force dropping {def.ViewElementDef.Name}" );
+               item.Drop( shared.FallDownItemContainerDef, actor );
+            }
+         }
+      }
+
+      /*
       if ( Config.Stript_Survivors ) TryPatch( typeof( GeoMission ), "ManageGear", nameof( BeforeManageGear_StriptItems ) );
       public static void BeforeManageGear_StriptItems ( GeoMission __instance, TacMissionResult result ) { try {
          var shared = SharedData.GetSharedDataFromGame();
