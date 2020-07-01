9b151e9e669805dcd384e5e9cac231a0ecf2dcc1
Sheep-y <your.sheepy@gmail.com>
2020-07-01 15:22:17 +0800

    Note how to loot missions.

--- Tactical/LootEverything/LootEverything.cs
+++ Tactical/LootEverything/LootEverything.cs
@@ -12,6 +12,8 @@ namespace Sheepy.PhoenixPt.LootEverything {
    internal class ModConfig {
       public bool Drop_All_Item = true;
       public bool Drop_All_Armour = true;
+      //public bool Loot_All_Missions = false; // GeoMission.ManageGear
+      //public bool Loot_On_Loss = false; // GeoMission.ManageGear
       //public bool Stript_Survivors = true;
       public int Config_Version = 20200630;
    }
